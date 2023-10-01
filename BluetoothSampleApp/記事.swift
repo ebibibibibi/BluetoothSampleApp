2a581d219dd97d1d3b0bd6eb678b72b25ae67241
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2023-10-01 09:30:45 +0900

    🐰UPDATE: ディレクトリ構造を整理

---
+++ BluetoothSampleApp/記事.swift
@@ -0,0 +1,14 @@
+//
+//  メモ用.swift
+//  BluetoothSampleApp
+//
+//  Created by kotomi takahashi on 2023/09/25.
+//
+
+import Foundation
+
+/*
+ https://qiita.com/moaible/items/84e5789342840d862305
+ - CBUUID:
+ サービスが持つID
+ */
