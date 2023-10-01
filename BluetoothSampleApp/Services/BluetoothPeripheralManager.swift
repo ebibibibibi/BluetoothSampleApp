0007cc1d70c2e249ec76e712b820c5e8a4f50dde
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2023-10-02 00:05:42 +0900

    ADD: BluetoothPeripheralManager 接続とイベント処理ロジックを追加

--- BluetoothSampleApp/Services/BluetoothPeripheralManager.swift
+++ BluetoothSampleApp/Services/BluetoothPeripheralManager.swift
@@ -10,23 +10,25 @@ import CoreBluetooth
 
 /// Bluetooth接続に伴い、クライアントとしての統制を行うclass
 final class BluetoothPeripheralManager: NSObject {
-
+    
     // ペリフェラルの状態を管理する Core Bluetooth object
     private var peripheralManager: CBPeripheralManager?
-
+    
+    // チャットデータのフローを制御するサービス内のcharacteristic
+    private var characteristic: CBMutableCharacteristic?
     // アドバタイズが延長されているかどうか
     private var advertPending = false
-
+    
     public func start() {
         if peripheralManager == nil {
             peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
         }
-
+        
         guard peripheralManager?.state == .poweredOn else {
             advertPending = true
             return
         }
-
+        
         startAdvertising()
     }
 }
@@ -34,16 +36,17 @@ final class BluetoothPeripheralManager: NSObject {
 extension BluetoothPeripheralManager {
     private func startAdvertising() {
         guard !(peripheralManager?.isAdvertising ?? true) else { return }
-
+        
         let advertisementData: [String: Any] = [CBAdvertisementDataLocalNameKey: "XD",
-                                                 CBAdvertisementDataServiceUUIDsKey: [BluetoothService.chatServiceID]]
-
+                                             CBAdvertisementDataServiceUUIDsKey: [BluetoothService.chatID]]
+        
         peripheralManager?.startAdvertising(advertisementData)
         advertPending = false
     }
 }
 
 extension BluetoothPeripheralManager: CBPeripheralManagerDelegate {
+    /// Bluetoothがどの状態に入ったかを表示
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
         switch peripheral.state {
         case .unknown:
@@ -61,6 +64,22 @@ extension BluetoothPeripheralManager: CBPeripheralManagerDelegate {
             if advertPending { startAdvertising() }
         @unknown default:
             print("peripheralの状態が`an unknown state`に変更されました")
+        }
+        // 電源がオンになったら、ペリフェラルをサポートするサービスとキャラクタリスティックを構成
+        guard peripheral.state == .poweredOn else { return }
+        
+        characteristic = CBMutableCharacteristic(type: BluetoothCharacteristic.chatID,
+                                                 properties: .notify,
+                                                 value: nil,
+                                                 permissions: .writeable)
+        // キャラクタリスティックを表現するサービスを作成
+        let service = CBMutableService(type: BluetoothService.chatID, primary: true)
+        service.characteristics = [self.characteristic!]
+        // ペリフェラルにこのサービスを登録
+        peripheralManager?.add(service)
+        // Bluetoothがセットアップを開始する前に、すでにアドバタイズのリクエストを行っていた場合、開始する。
+        if advertPending {
+            startAdvertising()
         }
     }
 }
