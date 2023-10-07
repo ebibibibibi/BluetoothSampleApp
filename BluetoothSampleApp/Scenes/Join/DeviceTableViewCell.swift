923b10cb0660c43a9651e23750339cae7373fc0c
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2023-10-07 23:29:09 +0900

    🐣ADD: Added device table view cell class

---
+++ BluetoothSampleApp/Scenes/Join/DeviceTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceTableViewCell.swift
+//  BluetoothSampleApp
+//
+//  Created by kotomi takahashi on 2023/10/07.
+//
+
+import UIKit
+
+class DeviceTableViewCell: UITableViewCell {
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+
+
+}
