da62a473d821967aa47cbe0217886a22a22bbc49
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2023-10-07 23:18:47 +0900

    🐣ADD: table view logic for the join scene

--- BluetoothSampleApp/Scenes/Join/JoinViewController.swift
+++ BluetoothSampleApp/Scenes/Join/JoinViewController.swift
@@ -7,18 +7,50 @@
 
 import UIKit
 
-class JoinViewController: UITableViewController {
+struct Sections {
+    static let name = 0
+    static let availableDevices = 1
+}
 
+class JoinViewController: UITableViewController {
+    static let deviceCellIdentifier = "DeviceCell"
+    static let nameCellIdentifier = "NameCell"
+    
+    // MARK: - Class Creation -
+    init() {
+        super.init(style: .insetGrouped)
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: JoinViewController.deviceCellIdentifier)
+        tableView.register(UINib(nibName: "TextFieldTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: JoinViewController.nameCellIdentifier)
     }
-
+    
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 2
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        if section == Sections.name { return 1 }
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == Sections.name {
+            let cell = tableView.dequeueReusableCell(withIdentifier: JoinViewController.nameCellIdentifier,
+                                                     for: indexPath)
+            return cell
+        }
+        
+        return tableView.dequeueReusableCell(withIdentifier: JoinViewController.deviceCellIdentifier, for: indexPath)
     }
 }
