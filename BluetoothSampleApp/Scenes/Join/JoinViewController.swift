fe7c898da80a67d8a195a8d3b28b9d4e79ecec85
ebibibibibi <91860481+ebibibibibi@users.noreply.github.com>
2023-10-07 22:50:16 +0900

    🐣ADD:  join view controller class

---
+++ BluetoothSampleApp/Scenes/Join/JoinViewController.swift
@@ -0,0 +1,26 @@
+//
+//  JoinViewController.swift
+//  BluetoothSampleApp
+//
+//  Created by kotomi takahashi on 2023/10/07.
+//
+
+import Foundation
+
+import UIKit
+
+class JoinViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
